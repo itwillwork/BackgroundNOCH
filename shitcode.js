0b5af8c4a24762e0d71cc5ccc6d3f34732fa883c
itwillwork <nurullin_edgar@mail.ru>
2016-02-28 21:02:44 +0300

    hotfix

--- shitcode.js
+++ shitcode.js
@@ -1,4 +1,3 @@
-//TODO масштабирование
 /*
  =============================================================================
  Данные для отладки
@@ -17,17 +16,25 @@ var playerPosition = {
     x: 5250,
     y: 3400
 };
-var coefScale = 1;
 //эмуляция ресайза
+
+var coefScale = 1;
 setTimeout(function () {
-    coefScale =  0.25;
-    /*
     setInterval(function () {
         if (coefScale > 0.25) {
             coefScale -= 0.05;
         }
     }, 27);
-    */
+},1000);
+
+/*
+var coefScale = 0.25;
+setTimeout(function () {
+    setInterval(function () {
+        if (coefScale < 1) {
+            coefScale += 0.05;
+        }
+    }, 27);
 },1000);
 /*
  =============================================================================
@@ -53,21 +60,10 @@ Background.prototype.tick = function () {
     // TODO удалить
     // Данные для отладки, чтобы игрок двигался
     // дергается потому что рандом, скорость каждый раз изменяется
-    /*
-    //random mode
-    playerPosition.x += Math.random();
-    playerPosition.y += Math.random();
-    */
 
     //slowmode
     playerPosition.x += 0.5;
     playerPosition.y -= 0.5;
-
-    /*
-    //fast mode
-    playerPosition.x += 5;
-    playerPosition.y += 5;
-    */
 
     var deltaPlayerPosition = {
             x: playerPosition.x - this.prevPlayerPosition.x,
@@ -93,7 +89,6 @@ function CollectionBackgroundItem() {
         playerPosition.x + gameSize.x, playerPosition.y + gameSize.y);
 }
 CollectionBackgroundItem.prototype.filling = function (minX, minY, maxX, maxY) {
-    console.log(minX, minY, maxX, maxY);
     var baseCatalog = "images/",
         collectionOfImages = [
             {
@@ -114,20 +109,17 @@ CollectionBackgroundItem.prototype.filling = function (minX, minY, maxX, maxY) {
         //потому что нужно захватить еще и не отображаемые поля
         var count = Math.floor((maxX - minX) * (maxY - minY) / (this.densityOfLevel[i] * coefScale)),
             oldLengthCollection = this.collectionOfLevels[i].length;
-        console.log(count);
         while (this.collectionOfLevels[i].length < count + oldLengthCollection) {
             //пришлось манипулировать с единицами так, как номера картинок начинаются с 1, а не с 0
             var randomIndexImage  = Math.floor( Math.random() * (collectionOfImages[i].count - 1) + 1),
                 randomImageSrc = baseCatalog + collectionOfImages[i].prefix + randomIndexImage + ".png",
                 randomPosition = {
-                    x: Math.floor(Math.random() * (maxX - minX)) + minX, //Math.floor( Math.random() * (maxX - minX) + minX),
-                    y: Math.floor(Math.random() * (maxY - minY)) + minY //Math.floor( Math.random() * (maxY - minY) + minY)
+                    x: Math.floor(Math.random() * (maxX - minX)) + minX,
+                    y: Math.floor(Math.random() * (maxY - minY)) + minY
                 };
             this.collectionOfLevels[i].push( new BackgroundItem(randomPosition, randomImageSrc, this.coefForMovie[i]) );
-            //console.log(randomPosition);
         }
     }
-    console.log();
 };
 CollectionBackgroundItem.prototype.tick = function (stage, deltaPlayerPosition, deltaCoefScale) {
     if (deltaCoefScale) {
@@ -143,58 +135,63 @@ CollectionBackgroundItem.prototype.tick = function (stage, deltaPlayerPosition, 
         }
     }
 };
-CollectionBackgroundItem.prototype.holdIfGetOut = function ( nodeBackground ) {
-    if (nodeBackground.position.x <= playerPosition.x - gameSize.x) {
-        nodeBackground.position.x += 2 * gameSize.x;
+CollectionBackgroundItem.prototype.holdIfGetOut = function ( itemBackground ) {
+    if (itemBackground.position.x <= playerPosition.x - gameSize.x) {
+        itemBackground.position.x += 2 * gameSize.x;
     }
-    if (nodeBackground.position.x >= playerPosition.x + gameSize.x) {
-        nodeBackground.position.x -= 2 * gameSize.x;
+    if (itemBackground.position.x >= playerPosition.x + gameSize.x) {
+        itemBackground.position.x -= 2 * gameSize.x;
     }
-    if (nodeBackground.position.y <= playerPosition.y - gameSize.y) {
-        nodeBackground.position.y +=  2 * gameSize.y;
+    if (itemBackground.position.y <= playerPosition.y - gameSize.y) {
+        itemBackground.position.y +=  2 * gameSize.y;
     }
-    if (nodeBackground.position.y >= playerPosition.y + gameSize.y) {
-        nodeBackground.position.y -=  2 * gameSize.y;
+    if (itemBackground.position.y >= playerPosition.y + gameSize.y) {
+        itemBackground.position.y -=  2 * gameSize.y;
     }
 };
 CollectionBackgroundItem.prototype.zoomGame = function (deltaCoefScale) {
+    for (var i = 0; i < this.collectionOfLevels.length; i++) {
+        for (var j = 0; j < this.collectionOfLevels[i].length; j++) {
+            var newPosition = {
+                x: ((this.collectionOfLevels[i][j].position.x - playerPosition.x) / (deltaCoefScale + coefScale)) * coefScale + playerPosition.x,
+                y: ((this.collectionOfLevels[i][j].position.y - playerPosition.y) / (deltaCoefScale + coefScale)) * coefScale + playerPosition.y,
+            };
+            this.collectionOfLevels[i][j].position = newPosition;
+        }
+    }
     if (deltaCoefScale > 0) {
         //уменьшение
-        for (var i = 0; i < this.collectionOfLevels.length; i++) {
-            for (var j = 0; j < this.collectionOfLevels[i].length; j++) {
-                var newPosition = {
-                    x: ((this.collectionOfLevels[i][j].position.x - playerPosition.x) / (deltaCoefScale + coefScale)) * coefScale + playerPosition.x,
-                    y: ((this.collectionOfLevels[i][j].position.y - playerPosition.y) / (deltaCoefScale + coefScale)) * coefScale + playerPosition.y,
-                };
-                this.collectionOfLevels[i][j].position = newPosition;
-            }
-        }
         var newMinX = playerPosition.x - gameSize.x,
             newMinY = playerPosition.y - gameSize.y,
             newMaxX = playerPosition.x + gameSize.x,
             newMaxY = playerPosition.y + gameSize.y,
-
-
             oldMinX = (((playerPosition.x - gameSize.x) - playerPosition.x) / (deltaCoefScale + coefScale)) * coefScale + playerPosition.x,
             oldMinY = (((playerPosition.y - gameSize.y) - playerPosition.y) / (deltaCoefScale + coefScale)) * coefScale + playerPosition.y,
             oldMaxX = (((playerPosition.x + gameSize.x) - playerPosition.x) / (deltaCoefScale + coefScale)) * coefScale + playerPosition.x,
             oldMaxY = (((playerPosition.y + gameSize.y) - playerPosition.y) / (deltaCoefScale + coefScale)) * coefScale + playerPosition.y;
-        /*
-        console.log(newMinX, newMinY, newMaxX, newMaxY);
-        console.log(oldMinX, oldMinY, oldMaxX, oldMaxY);
-        console.log("======")
-        console.log(newMinX, oldMinY, oldMinX, oldMaxY);
-        console.log(newMinX, oldMaxY, newMaxX, newMaxY);
-        console.log(newMinX, newMinY, newMaxX, oldMinY);
-        console.log(oldMaxX, oldMinY, newMaxX, oldMaxY);
-        */
         this.filling(newMinX, oldMinY, oldMinX, oldMaxY);
         this.filling(newMinX, oldMaxY, newMaxX, newMaxY);
         this.filling(newMinX, newMinY, newMaxX, oldMinY);
         this.filling(oldMaxX, oldMinY, newMaxX, oldMaxY);
     } else {
-
+        //уменьшение
+        for (var i = 0; i < this.collectionOfLevels.length; i++) {
+            for (var j = 0; j < this.collectionOfLevels[i].length; j++) {
+                var itemBackground = this.collectionOfLevels[i][j];
+                if ((itemBackground.position.x <= playerPosition.x - gameSize.x)
+                    || (itemBackground.position.x >= playerPosition.x + gameSize.x)
+                    || (itemBackground.position.y <= playerPosition.y - gameSize.y)
+                    || (itemBackground.position.y >= playerPosition.y + gameSize.y))
+                {
+                    this.deleteNode(i, j);
+                }
+            }
+        }
     }
+};
+CollectionBackgroundItem.prototype.deleteNode = function (layerIndex, itemIndex) {
+    if (itemIndex < 0) return;
+    this.collectionOfLevels[layerIndex].splice(itemIndex, 1);
 };
 function BackgroundItem(position, src, coefForMove) {
     var MAX_VELOCITY = 0.3,
@@ -231,10 +228,12 @@ BackgroundItem.prototype.isVisible = function (positionOnCanvas) {
     if (((positionOnCanvas.x - this.bitmap.width) >= gameSize.x) || ((positionOnCanvas.y - this.bitmap.height) >= gameSize.y)) {
         return false;
     }
+    //TODO разобраться с оптимизацией, не получлось вернуть размер картинки
+    /*
     //за верхней или левой границей
     if (((positionOnCanvas.x + this.bitmap.width) <= 0) || ((positionOnCanvas.y + this.bitmap.height) <= 0)) {
         return false;
-    }
+    }*/
     return true;
 };
 BackgroundItem.prototype.draw = function (context) {
@@ -243,8 +242,10 @@ BackgroundItem.prototype.draw = function (context) {
             y: this.position.y - (playerPosition.y - gameSize.y / 2)
         },
         bitmap;
+
     //если объект не виден на экране не рисуем его
     if (!this.isVisible(positionOnCanvas)) return;
+
     bitmap = this.bitmap;
     bitmap.x = positionOnCanvas.x;
     bitmap.y = positionOnCanvas.y;
